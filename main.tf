79f1814299267234b1f152b1805c96bd3ab8455c
Xavier Chotard <soyouz@src386.org>
2021-04-19 23:28:27 +0200

    placementgroup and privatenic support

--- main.tf
+++ main.tf
@@ -9,6 +9,13 @@ resource "scaleway_instance_ip" "ip" {
   count        = var.nb_instances * (var.enable_public_ip == true ? 1 : 0)
 }
 
+# Create and attach to a VPC (if private_network_id is defined)
+resource "scaleway_instance_private_nic" "nic" {
+  count                 = var.nb_instances * (var.private_network_id != "" ? 1 : 0)
+  server_id             = scaleway_instance_server.instance[count.index].id
+  private_network_id    = var.private_network_id
+}
+
 # ========================================================= #
 #                                                           #
 #                       Instance                            #
@@ -16,14 +23,20 @@ resource "scaleway_instance_ip" "ip" {
 # ========================================================= #
 
 resource "scaleway_instance_server" "instance" {
-
   count                 = var.nb_instances
+
+  # Mandatory
+  image                 = var.image_id
   name                  = "${var.name}-${count.index}"
   type                  = var.type
-  image                 = var.image_id
-  security_group_id     = var.security_group_id
+
+  # Optionnal
   additional_volume_ids = var.additional_volume_ids
-  ip_id                 = scaleway_instance_ip.ip[count.index].id
-  tags                  = var.tags
   cloud_init            = var.cloud_init
+  enable_ipv6           = var.enable_ipv6
+  ip_id                 = scaleway_instance_ip.ip[count.index].id
+  placement_group_id    = var.placement_group_id
+  security_group_id     = var.security_group_id
+  tags                  = var.tags
+  
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,83 @@
-variable "image_id" {}
-variable "nb_instances" {
-    type = number
+# ========================================================= #
+#                                                           #
+#                  Mandatory variables                      #
+#                                                           #
+# ========================================================= #
+
+variable "image_id" {
+  description   = "Image ID. See README."
+  type          = string
+  default       = null
 }
-variable "enable_public_ip" {}
-variable "name" {}
-variable "type" {}
-variable "security_group_id" {
-    default = null
+
+variable "name" {
+  description   = "Name of the instance."
+  type          = string
+  default       = null
 }
+
+variable "type" {
+  description   = "Instance SKU (DEV1-S, GP1-S...)"
+  type          = string
+  default       = null
+}
+
+# ========================================================= #
+#                                                           #
+#                  Optionnal variables                      #
+#                                                           #
+# ========================================================= #
+
 variable "additional_volume_ids" {
-    type    = list(string)
-    default = null
+  description   = "Data disks to attach."
+  type          = list(string)
+  default       = null
 }
-variable "tags" {
-    default = null
-    type    = list(string)
-}
+
 variable "cloud_init" {
-    default = null
+  description   = "Cloud init to provision the instance with."
+  type          = string
+  default       = null
+}
+
+variable "enable_ipv6" {
+  description   = "Enable IPv6 address."
+  type          = bool
+  default       = false
+}
+
+variable "enable_public_ip" {
+  description   = "Provision and enable a Public IP."
+  type          = bool
+  default       = true
+}
+
+variable "nb_instances" {
+  description   = "Number of instances."
+  type          = number
+  default       = 1
+}
+
+variable "placement_group_id" {
+  description   = "Placement group ID."
+  type          = string
+  default       = null
+}
+
+variable "private_network_id" {
+  description   = "VPC ID."
+  type          = string
+  default       = ""
+}
+
+variable "security_group_id" {
+  description   = "Security group ID."
+  type          = string
+  default       = null
+}
+
+variable "tags" {
+  description   = "A list of tags to add."
+  type          = list(string)
+  default       = null
 }
